42f521b8f838c85dfe7538a0c9ee85426952e43c
Rajath K Naik <rajathkrishnamurthy173@gmail.com>
2024-11-17 15:13:45 +0530

    Update g45.c

--- g45.c
+++ g45.c
@@ -53,3 +53,20 @@ int main() {
 
     return 0;
 }
+//other method
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);  // Print numbers from 1 to i for each row
+        }
+        printf("\n");  // Move to the next line after each row
+    }
+
+    return 0;
+}
